02a0309123fe6511cae4cd0ddc67533dc7c2a16c
Jessica <jessicamwangi933@gmail.com>
2021-12-13 09:40:40 +0300

    added function varrables let if and else if to the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 function getAkanName(){
-  
+  var year = document.getElementById("year").value;
+  var month = document.getElementById("month").value;
+  var day = document.getElementById("day").value;
+  var gender = document.getElementById ("gender").value;
+  let birthday = new Date(`${year}-${month}-${day}`)
+  let dateString = birthday.toDateString()
+  let birthDate = birthday.getDay()
+  if (gender == "male"){
+    let name = maleNames[birthDate]
+    alert ("You were born on" +  dateString + "Your Akan Name is" + name)
+  }
+  else if (gender == "female"){
+    let name = femaleNames[birthDate]
+    alert ("You were born on" +  dateString + "Your Akan Name is" + name)
+  }
+  else {
+    alert("Please select gender")
+  }    
+
+  if (month > 12 || month < 1){
+     alert ("Invalid month")
+  }
+  if (day > 31 || month <1){
+    alert ("Invalid date")
+  }
 }
