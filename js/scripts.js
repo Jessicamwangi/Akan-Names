b8db06fd1e0bad24f58434b13b2150959e8da322
Jessica <jessicamwangi933@gmail.com>
2021-12-13 09:23:25 +0300

    added variables to the js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+function getAkanName(){
+  
+}
