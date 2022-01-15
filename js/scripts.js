612d0b4e9c633642fca501666be662815afa8606
Jessica <jessicamwangi933@gmail.com>
2022-01-15 20:41:39 +0300

    re-done js with new formular

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,105 @@
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-function getAkanName(){
-  var year = document.getElementById("year").value;
-  var month = document.getElementById("month").value;
+function getAkanName() {
   var day = document.getElementById("day").value;
-  var gender = document.getElementById ("gender").value;
-  let birthday = new Date(`${year}-${month}-${day}`)
-  let dateString = birthday.toDateString()
-  let birthDate = birthday.getDay()
-  if (gender == "male"){
-    let name = maleNames[birthDate]
-    alert ("You were born on " +  dateString + " Your Akan Name is " + name)
-  }
-  else if (gender == "female"){
-    let name = femaleNames[birthDate]
-    alert ("You were born on " +  dateString + " Your Akan Name is " + name)
-  }
-  else {
-    alert("Please select gender")
-  }    
+  var month = document.getElementById("month").value;
+  var year = document.getElementById("year").value;
+  var genders = document.getElementById("genders");
+  var gender = genders.elements["gender"].value;
 
-  if (month > 12 || month < 1){
-     alert ("Enter correct month")
+  if (day <= 0 || day > 31) {
+    alert("Please Enter a Valid Date");
+    return;
+
   }
-  if (day > 31 || month <1){
-    alert ("Enter correct day")
+  else if (month <= 0 || month > 12) {
+    alert("Please Enter a valid Month")
+    return;
+  };
+
+  var c = year.slice(0, 2);
+  var cc = parseInt(c);
+  var y = year.slice(2, 5);
+  var yy = parseInt(y);
+  var mm = parseInt(month);
+  var dd = parseInt(day);
+  var dayOfWeek = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+  if (gender === "Female") {
+    function female() {
+        var d = dayOfWeek.toFixed();
+        var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        switch (d) {
+            case '1':
+                alert("Your Akan name is: " + female[0]);
+                break;
+
+            case '2':
+                alert("Your Akan name is: " + female[1]);
+                break;
+
+            case '3':
+                alert("Your Akan name is: " + female[2]);
+                break;
+
+            case '4':
+                alert("Your Akan name is: " + female[3]);
+                break;
+
+            case '5':
+                alert("Your Akan name is: " + female[4]);
+                break;
+
+            case '6':
+                alert("Your Akan name is: " + female[5]);
+                break;
+
+            case '0':
+                alert("Your Akan name is: " + female[6]);
+                break;
+        }
+    }
+    female();
   }
-}
+  if (gender === "Male") {
+    function male() {
+      var d = dayOfWeek.toFixed();
+      var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+      switch (d) {
+          case '1':
+              alert("Your Akan name is: " + male[0]);
+              break;
+
+          case '2':
+              alert("Your Akan name is: " + male[1]);
+              break;
+
+          case '3':
+              alert("Your Akan name is: " + male[2]);
+              break;
+
+          case '4':
+              alert("Your Akan name is: " + male[3]);
+              break;
+
+          case '5':
+              alert("Your Akan name is: " + male[4]);
+              break;
+
+          case '6':
+              alert("Your Akan name is: " + male[5]);
+              break;
+
+          case '0':
+              alert("Your Akan name is: " + male[6]);
+              break;
+      }
+    }
+
+    male();
+  }
+  if (gender === "") {
+    alert("Please Choose A Gender");
+  }
+
+
+
+};
